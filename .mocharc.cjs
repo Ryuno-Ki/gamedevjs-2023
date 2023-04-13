141913b1926fab586f05a9ad0d052028231dcd90
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-13 20:10:03 +0200

    style: configure tooling
    This should enforce my commit messages, while also setting up mocha for
    running tests.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ .mocharc.cjs
@@ -0,0 +1,11 @@
+// See https://github.com/mochajs/mocha/blob/master/example/config/.mocharc.js
+module.exports = {
+  bail: true,
+  'check-leaks': true,
+  'forbid-only': true,
+  'forbid-pending': true,
+  global: [
+  ],
+  parallel: true,
+  recursive: true,
+}
