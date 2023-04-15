7123903eaf2ca36d1a9a57737553617bf06f16c2
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-15 17:56:02 +0200

    feat: link to other scenes from title
    I derive the transitions from the state now.
    I will likely need to extract the logic into a helper function soon to
    reuse it on other scenes.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/client/components/scenes/title.js
+++ src/client/components/scenes/title.js
@@ -1,4 +1,61 @@
+/** @typedef {import('../scenes/index').Scene} Scene */
 /** @typedef {import('../../state/initial').State} State */
+
+/**
+ * Extracts the transitions from title scene from state.
+ *
+ * @private
+ * @param {State} state
+ * @returns {Array<Scene>}
+ */
+function getTransitionsFromState (state) {
+  const { scenes: { states: { title: titleState } } } = state
+  const transitionEvents = titleState ? titleState.on : []
+  if (!transitionEvents) {
+    console.info('Expected transitions from title scene but found not')
+    return []
+  }
+
+  const transitions = Object
+    .values(transitionEvents)
+    .map((event) => event.target)
+  return transitions
+}
+
+/**
+ * Turns transitions into anchor elements.
+ *
+ * @private
+ * @param {Array<Scene>} transitions
+ * @returns {Array<HTMLAnchorElement>}
+ */
+function mapTransitionsToLinks (transitions) {
+  return transitions.map((transition) => {
+    const anchor = document.createElement('a')
+    anchor.href = `#${transition}`
+    anchor.textContent = transition
+    return anchor
+  })
+}
+
+/**
+ * Build the DOM to attach to the target element.
+ *
+ * @private
+ * @param {State} state
+ * @returns {HTMLDivElement}
+ */
+function buildTitleScene (state) {
+  const container = document.createElement('div')
+  const headline = document.createElement('h1')
+  headline.textContent = 'Title Scene'
+
+  const transitions = getTransitionsFromState(state)
+  const anchors = mapTransitionsToLinks(transitions)
+  container.appendChild(headline)
+  anchors.forEach((anchor) => container.appendChild(anchor))
+  return container
+}
 
 /**
  * Renders the scene for title.
@@ -13,7 +70,9 @@ export function titleSceneComponent (targetElement, state) {
   if (state.activeScene !== 'title') {
     element.innerHTML = ''
   } else {
-    element.innerHTML = 'Title Scene'
+    const child = buildTitleScene(state)
+    // TODO: Think about how to use .replaceWith but keep it idempotent
+    element.innerHTML = child.outerHTML
   }
 
   return element
--- test/client/components/scenes/title.test.js
+++ test/client/components/scenes/title.test.js
@@ -1,10 +1,22 @@
-import { expect } from 'chai'
+import chai from 'chai'
+import chaiDom from 'chai-dom'
 
 import { document } from '../../../prepare.js'
 import { initialState } from '../../../../src/client/state/initial.js'
 import { titleSceneComponent } from '../../../../src/client/components/scenes/title.js'
 
+chai.use(chaiDom)
+const { expect } = chai
+
 describe('titleSceneComponent', () => {
+  beforeEach(() => {
+    global.document = document
+  })
+
+  afterEach(() => {
+    delete global.document
+  })
+
   describe('when title scene is active', () => {
     it('should render the title scene', () => {
       // Arrange
@@ -17,6 +29,17 @@ describe('titleSceneComponent', () => {
       // Standard is tripped up by Chai here
       // eslint-disable-next-line no-unused-expressions
       expect(component.innerHTML).not.to.be.empty
+    })
+
+    it('should render links to transition to other scenes', () => {
+      // Arrange
+      const state = Object.assign({}, initialState, { activeScene: 'title' })
+
+      // Act
+      const component = titleSceneComponent(document.body, state)
+
+      // Assert
+      expect(component).to.contain('a')
     })
   })
 
