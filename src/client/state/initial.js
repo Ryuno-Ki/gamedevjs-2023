8b821dc177e2f92d02c631d57a45f3035a41c6fa
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-15 17:27:06 +0200

    feat: define scene transitions as XState FSM
    XState is a popular solution for finite state machines in JavaScript.
    I'm not sure whether I want to adopt the library, but I like the design
    of its architecture.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/client/state/initial.js
+++ src/client/state/initial.js
@@ -2,15 +2,91 @@
 /** @typedef {'UNINITIALISED' | 'RUNNING'} GameStatus */
 
 /**
+ * @typedef {Object.<'target', Scene>} Transition
+ */
+
+/**
+ * @typedef {Object.<string, Transition>} TransitionEvent
+ */
+
+/**
+ * @typedef {object} FiniteState
+ * @property {TransitionEvent} [on] - Possible state transitions
+ */
+
+/**
+ * @typedef {Object.<string, FiniteState | undefined>} FiniteStates
+ */
+
+/**
+ * XState definition for scenes.
+ *
+ * @typedef {object} Scenes
+ * @property {string} id - ID of state machine
+ * @property {Scene} initial - Initial state in state machine
+ * @property {FiniteStates} states - State definition
+ * @see {@link https://xstate.js.org/docs/}
+ * @see {@link https://stately.ai/registry/new}
+ */
+
+/**
  * @typedef {object} State
  * @property {Scene} State.activeScene
  * @property {Array<number>} State.clock
  * @property {GameStatus} State.gameStatus
+ * @property {Scenes} State.scenes
  */
 
 /* @type {State} */
 export const initialState = {
   activeScene: 'title',
   clock: [],
-  gameStatus: 'UNINITIALISED'
+  gameStatus: 'UNINITIALISED',
+  scenes: {
+    id: 'XState definition for scenes',
+    predictableActionArguments: true,
+    preserveActionOrder: true,
+    initial: 'title',
+    states: {
+      about: {
+        on: {
+          navigateToTitle: {
+            target: 'title'
+          }
+        }
+      },
+      level: {
+      },
+      newGame: {
+        on: {
+          navigateToLevel: {
+            target: 'level'
+          },
+          navigateToTitle: {
+            target: 'title'
+          }
+        }
+      },
+      settings: {
+        on: {
+          navigateToTitle: {
+            target: 'title'
+          }
+        }
+      },
+      title: {
+        on: {
+          navigateToAbout: {
+            target: 'about'
+          },
+          navigateToNewGame: {
+            target: 'newGame'
+          },
+          navigateToSettings: {
+            target: 'settings'
+          }
+        }
+      }
+    }
+  }
 }
--- types/client/state/initial.d.ts
+++ types/client/state/initial.d.ts
@@ -2,6 +2,63 @@ export namespace initialState {
     const activeScene: string;
     const clock: never[];
     const gameStatus: string;
+    namespace scenes {
+        const id: string;
+        const predictableActionArguments: boolean;
+        const preserveActionOrder: boolean;
+        const initial: string;
+        namespace states {
+            namespace about {
+                namespace on {
+                    namespace navigateToTitle {
+                        const target: string;
+                    }
+                }
+            }
+            const level: {};
+            namespace newGame {
+                export namespace on_1 {
+                    export namespace navigateToLevel {
+                        const target_1: string;
+                        export { target_1 as target };
+                    }
+                    export namespace navigateToTitle_1 {
+                        const target_2: string;
+                        export { target_2 as target };
+                    }
+                    export { navigateToTitle_1 as navigateToTitle };
+                }
+                export { on_1 as on };
+            }
+            namespace settings {
+                export namespace on_2 {
+                    export namespace navigateToTitle_2 {
+                        const target_3: string;
+                        export { target_3 as target };
+                    }
+                    export { navigateToTitle_2 as navigateToTitle };
+                }
+                export { on_2 as on };
+            }
+            namespace title {
+                export namespace on_3 {
+                    namespace navigateToAbout {
+                        const target_4: string;
+                        export { target_4 as target };
+                    }
+                    namespace navigateToNewGame {
+                        const target_5: string;
+                        export { target_5 as target };
+                    }
+                    namespace navigateToSettings {
+                        const target_6: string;
+                        export { target_6 as target };
+                    }
+                }
+                export { on_3 as on };
+            }
+        }
+    }
 }
 /**
  * }
@@ -9,8 +66,41 @@ export namespace initialState {
  */
 export type Scene = import('../components/scenes/index').Scene;
 export type GameStatus = 'UNINITIALISED' | 'RUNNING';
+export type Transition = any;
+export type TransitionEvent = {
+    [x: string]: Transition;
+};
+export type FiniteState = {
+    /**
+     * - Possible state transitions
+     */
+    on?: {
+        [x: string]: any;
+    } | undefined;
+};
+export type FiniteStates = {
+    [x: string]: FiniteState | undefined;
+};
+/**
+ * XState definition for scenes.
+ */
+export type Scenes = {
+    /**
+     * - ID of state machine
+     */
+    id: string;
+    /**
+     * - Initial state in state machine
+     */
+    initial: Scene;
+    /**
+     * - State definition
+     */
+    states: FiniteStates;
+};
 export type State = {
     activeScene: Scene;
     clock: Array<number>;
     gameStatus: GameStatus;
+    scenes: Scenes;
 };
