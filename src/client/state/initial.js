d616ff39d9cf4e2655307cf76415cdfd7335928e
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-16 10:15:22 +0200

    refactor: use new module
    To finish up this small refactor I should import the new module and
    include it in the state definition.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/client/state/initial.js
+++ src/client/state/initial.js
@@ -1,33 +1,8 @@
-/** @typedef {import('../components/scenes/index').Scene} Scene}
+import { scenes } from './fsm/scenes.js'
+
+/** @typedef {import('../components/scenes/index').Scene} Scene} */
+/** @typedef {import('./fsm/scenes').Scenes} Scenes */
 /** @typedef {'UNINITIALISED' | 'INITIALISED' | 'RUNNING'} GameStatus */
-
-/**
- * @typedef {Object.<'target', Scene>} Transition
- */
-
-/**
- * @typedef {Object.<string, Transition>} TransitionEvent
- */
-
-/**
- * @typedef {object} FiniteState
- * @property {TransitionEvent} [on] - Possible state transitions
- */
-
-/**
- * @typedef {Object.<string, FiniteState | undefined>} FiniteStates
- */
-
-/**
- * XState definition for scenes.
- *
- * @typedef {object} Scenes
- * @property {string} id - ID of state machine
- * @property {Scene} initial - Initial state in state machine
- * @property {FiniteStates} states - State definition
- * @see {@link https://xstate.js.org/docs/}
- * @see {@link https://stately.ai/registry/new}
- */
 
 /**
  * @typedef {object} State
@@ -42,51 +17,5 @@ export const initialState = {
   activeScene: 'title',
   clock: [],
   gameStatus: 'UNINITIALISED',
-  scenes: {
-    id: 'XState definition for scenes',
-    predictableActionArguments: true,
-    preserveActionOrder: true,
-    initial: 'title',
-    states: {
-      about: {
-        on: {
-          navigateToTitle: {
-            target: 'title'
-          }
-        }
-      },
-      level: {
-      },
-      'new-game': {
-        on: {
-          navigateToLevel: {
-            target: 'level'
-          },
-          navigateToTitle: {
-            target: 'title'
-          }
-        }
-      },
-      settings: {
-        on: {
-          navigateToTitle: {
-            target: 'title'
-          }
-        }
-      },
-      title: {
-        on: {
-          navigateToAbout: {
-            target: 'about'
-          },
-          navigateToNewGame: {
-            target: 'new-game'
-          },
-          navigateToSettings: {
-            target: 'settings'
-          }
-        }
-      }
-    }
-  }
+  scenes
 }
--- types/client/state/initial.d.ts
+++ types/client/state/initial.d.ts
@@ -1,95 +1,19 @@
 export namespace initialState {
-    const activeScene: string;
-    const clock: never[];
-    const gameStatus: string;
-    namespace scenes {
-        const id: string;
-        const predictableActionArguments: boolean;
-        const preserveActionOrder: boolean;
-        const initial: string;
-        const states: {
-            about: {
-                on: {
-                    navigateToTitle: {
-                        target: string;
-                    };
-                };
-            };
-            level: {};
-            'new-game': {
-                on: {
-                    navigateToLevel: {
-                        target: string;
-                    };
-                    navigateToTitle: {
-                        target: string;
-                    };
-                };
-            };
-            settings: {
-                on: {
-                    navigateToTitle: {
-                        target: string;
-                    };
-                };
-            };
-            title: {
-                on: {
-                    navigateToAbout: {
-                        target: string;
-                    };
-                    navigateToNewGame: {
-                        target: string;
-                    };
-                    navigateToSettings: {
-                        target: string;
-                    };
-                };
-            };
-        };
-    }
+    export const activeScene: string;
+    export const clock: never[];
+    export const gameStatus: string;
+    export { scenes };
 }
 /**
  * }
- * /**
  */
 export type Scene = import('../components/scenes/index').Scene;
+export type Scenes = import('./fsm/scenes').Scenes;
 export type GameStatus = 'UNINITIALISED' | 'INITIALISED' | 'RUNNING';
-export type Transition = any;
-export type TransitionEvent = {
-    [x: string]: Transition;
-};
-export type FiniteState = {
-    /**
-     * - Possible state transitions
-     */
-    on?: {
-        [x: string]: any;
-    } | undefined;
-};
-export type FiniteStates = {
-    [x: string]: FiniteState | undefined;
-};
-/**
- * XState definition for scenes.
- */
-export type Scenes = {
-    /**
-     * - ID of state machine
-     */
-    id: string;
-    /**
-     * - Initial state in state machine
-     */
-    initial: Scene;
-    /**
-     * - State definition
-     */
-    states: FiniteStates;
-};
 export type State = {
     activeScene: Scene;
     clock: Array<number>;
     gameStatus: GameStatus;
     scenes: Scenes;
 };
+import { scenes } from './fsm/scenes.js';
