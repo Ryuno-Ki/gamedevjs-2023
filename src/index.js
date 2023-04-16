ecf1a46beef277e7eea92b172cee5f4721d01557
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-16 15:55:28 +0200

    refactor: call new function
    To finish up this refactoring I call the new module function.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import { levelSceneComponent } from './client/components/scenes/level.js'
 import { newGameSceneComponent } from './client/components/scenes/new-game.js'
 import { settingsSceneComponent } from './client/components/scenes/settings.js'
 import { titleSceneComponent } from './client/components/scenes/title.js'
+import { registerEventListeners } from './client/event-listener.js'
 import { add } from './client/registry.js'
 import { render } from './client/render.js'
-import { switchToScene } from './client/state/actions/switch-to-scene.js'
-import store from './client/state/store.js'
-
-/** @typedef {import('./client/components/scenes/index').Scene} Scene */
 
 add('about-scene', aboutSceneComponent)
 add('level-scene', levelSceneComponent)
@@ -17,32 +14,4 @@ add('settings-scene', settingsSceneComponent)
 add('title-scene', titleSceneComponent)
 
 render()
-
-document.body.addEventListener('click', async (event) => {
-  const { target } = event
-  if (!target) {
-    return
-  }
-
-  switch (/** @type {HTMLElement} */(target).nodeName) {
-    case 'A':
-      await handleLinkClick(/** @type {HTMLAnchorElement} */(target))
-      break
-    default:
-      // Do nothing
-  }
-})
-
-/**
- * Handle link clicks by transition to a new scene
- *
- * @private
- * @param {HTMLAnchorElement} anchorElement
- */
-async function handleLinkClick (anchorElement) {
-  const href = anchorElement.getAttribute('href')
-  if (href) {
-    const scene = /** @type {Scene} */(href.slice(1))
-    await store.dispatch(switchToScene(scene))
-  }
-}
+registerEventListeners()
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,1 @@
-export type Scene = import('./client/components/scenes/index').Scene;
+export {};
