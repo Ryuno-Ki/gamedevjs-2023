3da6e49c3f5ede9555a3cee18b7d8e28e1280c45
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-19 13:13:37 +0200

    chore: register win and gameover scene
    This way I can use them as components in my game.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import { aboutSceneComponent } from './client/components/scenes/about.js'
+import { gameoverSceneComponent } from './client/components/scenes/gameover.js'
 import { levelSceneComponent } from './client/components/scenes/level.js'
 import { newGameSceneComponent } from './client/components/scenes/new-game.js'
 import { settingsSceneComponent } from './client/components/scenes/settings.js'
 import { titleSceneComponent } from './client/components/scenes/title.js'
+import { winSceneComponent } from './client/components/scenes/win.js'
 import { registerEventListeners } from './client/event-listener.js'
 import { add } from './client/registry.js'
 import { render } from './client/render.js'
 import './index.css'
 
 add('about-scene', aboutSceneComponent)
+add('gameover-scene', gameoverSceneComponent)
 add('level-scene', levelSceneComponent)
 add('new-game-scene', newGameSceneComponent)
 add('settings-scene', settingsSceneComponent)
 add('title-scene', titleSceneComponent)
+add('win-scene', winSceneComponent)
 
 render()
 registerEventListeners()
