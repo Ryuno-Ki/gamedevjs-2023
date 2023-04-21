9073a2bc12adc6db1ccd729e57bc8f96265f69e2
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-21 14:53:36 +0200

    feat: make select-emoji action known to the rest of the game
    This way I can have type checks in place.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/client/state/actions/index.js
+++ src/client/state/actions/index.js
@@ -1,10 +1,12 @@
+export { selectEmoji } from './select-emoji.js'
 export { setIsBot } from './set-is-bot.js'
 export { setNickname } from './set-nickname.js'
 export { switchToScene } from './switch-to-scene.js'
 export { tick } from './tick.js'
 
-/** @typedef {import('./set-is-bot.js').Action} SetIsBotAction */
-/** @typedef {import('./set-nickname.js').Action} SetNicknameAction */
+/** @typedef {import('./select-emoji').Action} SelectEmojiAction */
+/** @typedef {import('./set-is-bot').Action} SetIsBotAction */
+/** @typedef {import('./set-nickname').Action} SetNicknameAction */
 /** @typedef {import('./switch-to-scene').Action} SwitchToSceneAction */
 /** @typedef {import('./tick').Action} TickAction */
-/** @typedef {SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction} Action */
+/** @typedef {SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction} Action */
--- types/client/state/actions/index.d.ts
+++ types/client/state/actions/index.d.ts
@@ -1,9 +1,11 @@
+export { selectEmoji } from "./select-emoji.js";
 export { setIsBot } from "./set-is-bot.js";
 export { setNickname } from "./set-nickname.js";
 export { switchToScene } from "./switch-to-scene.js";
 export { tick } from "./tick.js";
-export type SetIsBotAction = import('./set-is-bot.js').Action;
-export type SetNicknameAction = import('./set-nickname.js').Action;
+export type SelectEmojiAction = import('./select-emoji').Action;
+export type SetIsBotAction = import('./set-is-bot').Action;
+export type SetNicknameAction = import('./set-nickname').Action;
 export type SwitchToSceneAction = import('./switch-to-scene').Action;
 export type TickAction = import('./tick').Action;
-export type Action = SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction;
+export type Action = SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction;
