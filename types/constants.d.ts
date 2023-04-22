78fe4548ceddd4051e5e2b4da2565fb1ae53d24d
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-22 15:08:48 +0200

    docs: document purpose of constants
    This should help in understanding the code a bit better.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- types/constants.d.ts
+++ types/constants.d.ts
@@ -1,6 +1,12 @@
+/** CSS selector to find the part of a HTML document for the game. */
 export const APP_SELECTOR: "#app";
+/** Action type for selecting an emoji. */
 export const SELECT_EMOJI: "SELECT_EMOJI";
+/** Action type to toggle the type of player. */
 export const SET_IS_BOT: "SET_IS_BOT";
+/** Action type to update the nickname of a player. */
 export const SET_NICKNAME: "SET_NICKNAME";
+/** Action type to switch to anohter scene. */
 export const SWITCH_TO_SCENE: "SWITCH_TO_SCENE";
+/** Action type to update the game clock. */
 export const TICK: "TICK";
