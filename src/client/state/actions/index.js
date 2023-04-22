e5fb3f8c8b860aa4a38035830538b3afd47fb04d
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-22 15:31:16 +0200

    chore: announce new action to the game
    Otherwise TypeScript et al. won't notice.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/client/state/actions/index.js
+++ src/client/state/actions/index.js
@@ -1,12 +1,14 @@
+export { checkForWin } from './check-for-win.js'
 export { selectEmoji } from './select-emoji.js'
 export { setIsBot } from './set-is-bot.js'
 export { setNickname } from './set-nickname.js'
 export { switchToScene } from './switch-to-scene.js'
 export { tick } from './tick.js'
 
+/** @typedef {import('./check-for-win').Action} CheckForWinAction */
 /** @typedef {import('./select-emoji').Action} SelectEmojiAction */
 /** @typedef {import('./set-is-bot').Action} SetIsBotAction */
 /** @typedef {import('./set-nickname').Action} SetNicknameAction */
 /** @typedef {import('./switch-to-scene').Action} SwitchToSceneAction */
 /** @typedef {import('./tick').Action} TickAction */
-/** @typedef {SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction} Action */
+/** @typedef {CheckForWinAction | SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction} Action */
--- types/client/state/actions/index.d.ts
+++ types/client/state/actions/index.d.ts
@@ -1,11 +1,13 @@
+export { checkForWin } from "./check-for-win.js";
 export { selectEmoji } from "./select-emoji.js";
 export { setIsBot } from "./set-is-bot.js";
 export { setNickname } from "./set-nickname.js";
 export { switchToScene } from "./switch-to-scene.js";
 export { tick } from "./tick.js";
+export type CheckForWinAction = import('./check-for-win').Action;
 export type SelectEmojiAction = import('./select-emoji').Action;
 export type SetIsBotAction = import('./set-is-bot').Action;
 export type SetNicknameAction = import('./set-nickname').Action;
 export type SwitchToSceneAction = import('./switch-to-scene').Action;
 export type TickAction = import('./tick').Action;
-export type Action = SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction;
+export type Action = CheckForWinAction | SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction;
