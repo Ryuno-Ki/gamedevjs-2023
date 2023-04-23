952e1db8422a6f7ec1d6ce42ed521bd87779a928
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-23 21:41:39 +0200

    feat: announce switch theme action to the app
    This way I can have type checks.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/client/state/actions/index.js
+++ src/client/state/actions/index.js
@@ -3,6 +3,7 @@ export { checkForWin } from './check-for-win.js'
 export { selectEmoji } from './select-emoji.js'
 export { setIsBot } from './set-is-bot.js'
 export { setNickname } from './set-nickname.js'
+export { switchTheme } from './switch-theme.js'
 export { switchToScene } from './switch-to-scene.js'
 export { tick } from './tick.js'
 
@@ -11,6 +12,7 @@ export { tick } from './tick.js'
 /** @typedef {import('./select-emoji').Action} SelectEmojiAction */
 /** @typedef {import('./set-is-bot').Action} SetIsBotAction */
 /** @typedef {import('./set-nickname').Action} SetNicknameAction */
+/** @typedef {import('./switch-theme').Action} SwitchThemeAction */
 /** @typedef {import('./switch-to-scene').Action} SwitchToSceneAction */
 /** @typedef {import('./tick').Action} TickAction */
-/** @typedef {CheckForGameoverAction | CheckForWinAction | SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction} Action */
+/** @typedef {CheckForGameoverAction | CheckForWinAction | SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchThemeAction | SwitchToSceneAction | TickAction} Action */
