7a549f00908a0e0cca1f88f11dbb95d04266d121
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-23 15:04:48 +0200

    feat: announce new action to the application
    This way I can have type checks.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/client/state/actions/index.js
+++ src/client/state/actions/index.js
@@ -1,3 +1,4 @@
+export { checkForGameover } from './check-for-gameover.js'
 export { checkForWin } from './check-for-win.js'
 export { selectEmoji } from './select-emoji.js'
 export { setIsBot } from './set-is-bot.js'
@@ -5,10 +6,11 @@ export { setNickname } from './set-nickname.js'
 export { switchToScene } from './switch-to-scene.js'
 export { tick } from './tick.js'
 
+/** @typedef {import('./check-for-gameover').Action} CheckForGameoverAction */
 /** @typedef {import('./check-for-win').Action} CheckForWinAction */
 /** @typedef {import('./select-emoji').Action} SelectEmojiAction */
 /** @typedef {import('./set-is-bot').Action} SetIsBotAction */
 /** @typedef {import('./set-nickname').Action} SetNicknameAction */
 /** @typedef {import('./switch-to-scene').Action} SwitchToSceneAction */
 /** @typedef {import('./tick').Action} TickAction */
-/** @typedef {CheckForWinAction | SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction} Action */
+/** @typedef {CheckForGameoverAction | CheckForWinAction | SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction} Action */
--- types/client/state/actions/index.d.ts
+++ types/client/state/actions/index.d.ts
@@ -1,13 +1,15 @@
+export { checkForGameover } from "./check-for-gameover.js";
 export { checkForWin } from "./check-for-win.js";
 export { selectEmoji } from "./select-emoji.js";
 export { setIsBot } from "./set-is-bot.js";
 export { setNickname } from "./set-nickname.js";
 export { switchToScene } from "./switch-to-scene.js";
 export { tick } from "./tick.js";
+export type CheckForGameoverAction = import('./check-for-gameover').Action;
 export type CheckForWinAction = import('./check-for-win').Action;
 export type SelectEmojiAction = import('./select-emoji').Action;
 export type SetIsBotAction = import('./set-is-bot').Action;
 export type SetNicknameAction = import('./set-nickname').Action;
 export type SwitchToSceneAction = import('./switch-to-scene').Action;
 export type TickAction = import('./tick').Action;
-export type Action = CheckForWinAction | SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction;
+export type Action = CheckForGameoverAction | CheckForWinAction | SelectEmojiAction | SetIsBotAction | SetNicknameAction | SwitchToSceneAction | TickAction;
