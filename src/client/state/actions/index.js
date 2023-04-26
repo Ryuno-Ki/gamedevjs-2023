e7bfe8a2a12bb1a559c08fa40e7c8ddeb08ceab3
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-26 16:21:43 +0200

    feat: announce action to the game
    This aids with type checking.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/client/state/actions/index.js
+++ src/client/state/actions/index.js
@@ -4,6 +4,7 @@ export { selectEmoji } from './select-emoji.js'
 export { selectWorld } from './select-world.js'
 export { setIsBot } from './set-is-bot.js'
 export { setNickname } from './set-nickname.js'
+export { setUseOpenMoji } from './set-use-open-moji.js'
 export { switchTheme } from './switch-theme.js'
 export { switchToScene } from './switch-to-scene.js'
 export { tick } from './tick.js'
@@ -14,7 +15,8 @@ export { tick } from './tick.js'
 /** @typedef {import('./select-world').Action} SelectWorldAction */
 /** @typedef {import('./set-is-bot').Action} SetIsBotAction */
 /** @typedef {import('./set-nickname').Action} SetNicknameAction */
+/** @typedef {import('./set-use-open-moji').Action} SetUseOpenMojiAction */
 /** @typedef {import('./switch-theme').Action} SwitchThemeAction */
 /** @typedef {import('./switch-to-scene').Action} SwitchToSceneAction */
 /** @typedef {import('./tick').Action} TickAction */
-/** @typedef {CheckForGameoverAction | CheckForWinAction | SelectEmojiAction | SelectWorldAction | SetIsBotAction | SetNicknameAction | SwitchThemeAction | SwitchToSceneAction | TickAction} Action */
+/** @typedef {CheckForGameoverAction | CheckForWinAction | SelectEmojiAction | SelectWorldAction | SetIsBotAction | SetNicknameAction | SetUseOpenMojiAction | SwitchThemeAction | SwitchToSceneAction | TickAction} Action */
