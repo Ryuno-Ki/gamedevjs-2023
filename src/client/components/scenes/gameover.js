7d5b3d018400d706ca09c94dd3c97ab32f7f7bb1
André Jaenisch <andre.jaenisch@posteo.de>
2023-04-26 13:27:17 +0200

    feat: display evaluation on gameover
    So in order to learn, I recapitulate the last game.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- src/client/components/scenes/gameover.js
+++ src/client/components/scenes/gameover.js
@@ -1,8 +1,11 @@
+import { openmojis } from '../../../vendor/openmoji.js'
+import { findTurnsPerRound } from '../../state/utils.js'
 import { buildAnchors } from '../anchors.js'
 import { el } from '../el.js'
 import { buildHeadline } from '../headline.js'
 
 /** @typedef {import('../../state/initial').State} State */
+/** @typedef {import('../../state/initial').World} World */
 
 /**
  * Renders the scene for gameover.
@@ -35,6 +38,48 @@ export function gameoverSceneComponent (targetElement, state) {
 function buildScene (state) {
   return /** @type {HTMLDivElement} */(el('div', [], {}, '', [
     buildHeadline('Game Over'),
+    buildEvaluation(state),
     buildAnchors(state, 'gameover')
   ]))
+}
+
+/**
+ * Evaluates the play and compares it with the solution.
+ *
+ * @private
+ * @param {State} state
+ * @returns {Array<*>}
+ */
+function buildEvaluation (state) {
+  const { activeWorld, players, worlds } = state
+
+  const turnsPerRound = findTurnsPerRound(state)
+  const play = turnsPerRound.map((round) => {
+    const children = round.map((turn, index) => {
+      const player = players[index]
+      const emojis = openmojis.find((emoji) => emoji.hexcode === turn)
+      const emoji = emojis ? emojis.emoji : 'Invalid'
+
+      const playerTurn = ['li', [], {}, '', [
+        ['span', [], {}, player.name],
+        ['span', [], {}, ' played '],
+        ['span', [], {}, emoji]
+      ]]
+      return ['ol', [], {}, '', [playerTurn]]
+    })
+
+    return ['li', [], {}, 'Round', children]
+  })
+
+  const world = /** @type {Array<World>} */(worlds).find((world) => world.id === activeWorld) || null
+  const solution = world !== null
+    ? world.solution.map((item) => {
+      const emojis = openmojis.find((emoji) => emoji.hexcode === item)
+      return emojis ? emojis.emoji : 'Invalid'
+    }).join(' ')
+    : 'Invalid'
+
+  return ['div', [], {}, solution, [[
+    'ol', [], {}, '', play
+  ]]]
 }
